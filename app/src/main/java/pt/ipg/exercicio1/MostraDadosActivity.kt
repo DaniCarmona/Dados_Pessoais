40acf650ca1dad9cf00a4d8050ab9ab996403c48
Daniel Carmona <carmona240101@hotmail.com>
2021-05-05 10:07:26 +0100

    dados passados para a segunda atividade

--- app/src/main/java/pt/ipg/exercicio1/MostraDadosActivity.kt
+++ app/src/main/java/pt/ipg/exercicio1/MostraDadosActivity.kt
@@ -13,16 +13,18 @@ class MostraDadosActivity : AppCompatActivity() {
         setContentView(R.layout.activity_mostra_dados)
 
 
-        val nome = intent.getStringExtra(MainActivity.INFO_EXTRA_MENSAGEM)
-        val email = intent.getSerializableExtra(MainActivity.INFO_EXTRA_DATA)
-        val telefone = intent.getSerializableExtra(MainActivity.INFO_EXTRA_DATA)
-        val idade = intent.getSerializableExtra(MainActivity.INFO_EXTRA_DATA)
+        val nome = intent.getStringExtra(MainActivity.INFO_EXTRA_NOME)
+        val email = intent.getStringExtra(MainActivity.INFO_EXTRA_EMAIL)
+        val telefone = intent.getStringExtra(MainActivity.INFO_EXTRA_TELEFONE)
+        val idade = intent.getIntExtra(MainActivity.INFO_EXTRA_IDADE, 0)
 
         val textViewNome= findViewById<TextView>(R.id.textViewNome)
         val textViewEmail= findViewById<TextView>(R.id.textViewEmail)
         val textViewTelefone = findViewById<TextView>(R.id.textViewTelefone)
         val textViewIdade= findViewById<TextView>(R.id.textViewIdade)
-        textViewMensagem.setText(mensagem)
-        textViewMensagem.setText(data.toString())
+        textViewNome.setText(nome)
+        textViewEmail.setText(email)
+        textViewTelefone.setText(telefone)
+        textViewIdade.setText(idade)
     }
 }
